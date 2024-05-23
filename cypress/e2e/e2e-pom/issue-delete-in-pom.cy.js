1d56fec7cd7999fbf166f35def2dac764cb437fe
maikoforcerebrum <sadomaiko@gmail.com>
2024-05-23 13:43:26 +0300

    My commets about the commit

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -3,23 +3,33 @@
  */
 import IssueModal from "../../pages/IssueModal";
 
-describe('Issue delete', () => {
+describe("Issue delete", () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
-    });
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .then((url) => {
+        //open issue detail modal with title from line 16
+        cy.contains(issueTitle).click();
+      });
   });
 
   //issue title, that we are testing with, saved into variable
-  const issueTitle = 'This is an issue of type: Task.';
+  const issueTitle = "This is an issue of type: Task.";
 
-  it('Should delete issue successfully', () => {
+  it("Should delete issue successfully", () => {
     //add steps to delete issue
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+    IssueModal.validateIssueVisibilityState(issueTitle, false);
   });
 
-  it('Should cancel deletion process successfully', () => {
+  it("Should cancel deletion process successfully", () => {
     //add steps to start deletion proces but cancel it
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+    IssueModal.validateIssueVisibilityState(issueTitle, true);
   });
 });
